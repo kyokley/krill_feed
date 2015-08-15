53a592753cae19027c806fd6aba5d82028bc1c4e
Kevin Yokley <kyokley2@gmail.com>
2015-08-15 08:10:25 -0500

    initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+virtualenv -p python2.7 ~/virtualenvs/krill
+source ~/virtualenvs/krill/bin/activate
+pip install -r requirements.txt
+deactivate
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ~/virtualenvs/krill/bin/activate
+krill -S ~/workspace/krill_feed/sources.txt -u 30
