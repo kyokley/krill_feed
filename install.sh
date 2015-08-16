1b27fe3517928b6781adf59261ba99c24933af6b
Kevin Yokley <kyokley2@gmail.com>
2015-08-15 22:37:40 -0500

    Default to auto-yes

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ virtualenv -p python2.7 ~/virtualenvs/krill
 source ~/virtualenvs/krill/bin/activate
 pip install -r requirements.txt
 deactivate
-sudo aptitude install elinks
+sudo aptitude install -y elinks
 sudo ln -s `pwd`/runelinks /usr/bin/runelinks
