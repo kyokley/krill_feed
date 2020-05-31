225dd89e4c5dcb2f5d48422bcf4baeb257fd4f0c
Kevin Yokley <kyokley2@gmail.com>
2020-05-31 07:06:54 -0500

    Remove obsolete install/run scripts

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-virtualenv -p python2.7 ~/virtualenvs/krill
-source ~/virtualenvs/krill/bin/activate
-pip install -r requirements.txt
-deactivate
-sudo aptitude install -y elinks
-sudo ln -s `pwd`/runelinks /usr/bin/runelinks
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source ~/virtualenvs/krill/bin/activate
-krill -S ~/workspace/krill_feed/sources.txt -u 30
